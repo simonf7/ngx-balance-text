568321da5e7e02e650ef15dc4825c663915b7fb3
Simon Foster <simonf7@hotmail.com>
2019-04-07 18:02:27 +0100

    Chore: replace accidentally removed module

---
+++ projects/ngx-balance-text/src/lib/ngx-balance-text.module.ts
@@ -0,0 +1,9 @@
+import { NgModule } from '@angular/core';
+import { NgxBalanceTextDirective } from './ngx-balance-text.directive';
+
+@NgModule({
+  declarations: [NgxBalanceTextDirective],
+  imports: [],
+  exports: [NgxBalanceTextDirective]
+})
+export class NgxBalanceTextModule {}
--- projects/ngx-balance-text/src/public-api.ts
+++ projects/ngx-balance-text/src/public-api.ts
@@ -3,3 +3,4 @@
  */
 
 export * from './lib/ngx-balance-text.directive';
+export * from './lib/ngx-balance-text.module';
