43eaaf86abbde5d8a6fa0761c5e0de5ed464b2f2
Simon Foster <simonf7@hotmail.com>
2019-04-19 17:16:05 +0100

    Feat: used lifecycle hooks to improve reliability of rendering

--- projects/ngx-balance-text/src/lib/ngx-balance-text.directive.ts
+++ projects/ngx-balance-text/src/lib/ngx-balance-text.directive.ts
@@ -1,11 +1,42 @@
-import { Directive, ElementRef } from '@angular/core';
+import {
+  Directive,
+  ElementRef,
+  Renderer2,
+  AfterViewChecked
+} from '@angular/core';
 import balanceText from 'balance-text';
 
 @Directive({
   selector: '[ngxBalanceText]'
 })
-export class NgxBalanceText {
-  constructor(el: ElementRef) {
-    balanceText(el.nativeElement, { watch: true });
+export class NgxBalanceText implements AfterViewChecked {
+  private className: string = null;
+
+  private uuidv4() {
+    return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
+      var r = (Math.random() * 16) | 0,
+        v = c == 'x' ? r : (r & 0x3) | 0x8;
+      return v.toString(16);
+    });
+  }
+
+  constructor(public el: ElementRef, public renderer: Renderer2) {}
+
+  ngOnInit() {
+    this.className = 'ngxBT-' + this.uuidv4();
+    this.renderer.addClass(this.el.nativeElement, this.className);
+    balanceText('.' + this.className, { watch: true });
+  }
+
+  ngAfterViewChecked() {
+    balanceText.updateWatched();
+  }
+
+  ngOnDestroy() {
+    // remove the class from the watched list
+    if (this.className) {
+      balanceText('.' + this.className, { watch: false });
+    }
+    this.className = null;
   }
 }
